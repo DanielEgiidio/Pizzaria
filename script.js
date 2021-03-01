9a9c20229443889e2d19740337bc8de3cb857daf
Daniel Egidio <danielegidioti@gmail.com>
2021-03-01 19:36:49 -0300

    Adaptação mobile

--- script.js
+++ script.js
@@ -101,7 +101,19 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     closeModal();
 });
 
+c('.menu-openner').addEventListener('click', ()=>{
+    if(cart.length > 0){
+        c('aside').style.left = '0'; 
+    }
+});
+c('.menu-closer').addEventListener('click', ()=>{
+    c('aside').style.left = '100vw'
+});
+
 function updateCart(){
+
+    c('.menu-openner span').innerHTML = cart.length;
+
     if(cart.length > 0) {
         c('aside').classList.add('show');
         c('.cart').innerHTML ='';
@@ -157,6 +169,7 @@ function updateCart(){
         c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
 
     }else {
-        c('aside').classList.add('remove');
+        c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
     }
 }
